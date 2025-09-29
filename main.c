d42f0238bf8ccf6dc06391ccb707ee03ff964932
Gustavo Francisco Toito <gustavotoito@gmail.com>
2025-09-29 13:32:19 +0000

    criação da função tradução

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 
 
-char separar (char entrada[], char separado[50][5]) {
+char separar (char entrada[], char separado[50][6]) {
     int b = 0, a = 0;
     for(int i = 0; entrada[i] != '\0'; i++) {
         // igual a diferente de 0, então não é espaço 
@@ -13,6 +13,7 @@ char separar (char entrada[], char separado[50][5]) {
             if (entrada[i+1] == ' ') {
                 a = 0;
                 b++;
+                i++;
                 separado[b][a++] = '|'; // marca espaço entre palavras
             }
             separado[b][a] = '\0';
@@ -22,19 +23,26 @@ char separar (char entrada[], char separado[50][5]) {
     }
 }
 
-int traducao() {
-
+int traducao(char separado[50][6], char banco[27][2][6], char traduzido[50][4]) {
+    for (int i = 0; i < 50; i++) {
+        for (int j = 0; j < 26; j++) {
+            if (strcmp(separado[i], banco[j][1]) == 0) {
+                strcpy(traduzido[i], banco[j][0]);
+            }
+            
+        }
+    }
 }
 
 void main() {
-    char banco_tradu[26][2][6] = {
+    char banco_tradu[27][2][6] = {
     {"A", ".-"},   {"B", "-..."}, {"C", "-.-."}, {"D", "-.."},
     {"E", "."},    {"F", "..-."}, {"G", "--."},  {"H", "...."},
     {"I", ".."},   {"J", ".---"}, {"K", "-.-"},  {"L", ".-.."},
     {"M", "--"},   {"N", "-."},   {"O", "---"},  {"P", ".--."},
     {"Q", "--.-"}, {"R", ".-."},  {"S", "..."},  {"T", "-"},
     {"U", "..-"},  {"V", "...-"}, {"W", ".--"},  {"X", "-..-"},
-    {"Y", "-.--"}, {"Z", "--.."}
+    {"Y", "-.--"}, {"Z", "--.."}, {"/", "|"}
     };
     /*for(int i = 0; i < 26; i++) {
         printf("%s = %s\n", banco_tradu[i][0], banco_tradu[i][1]);
@@ -45,10 +53,17 @@ void main() {
     char texto[100];
     printf("Digite o texto:");
     fgets(texto, 100, stdin);
-    char separado[50][5];
-
+    
+    char separado[50][6];
     separar (texto, separado);
     for(int i = 0; i < 50 && separado[i][0] != '\0'; i++) {
         printf("%s\n", separado[i]);
     }
+
+    char traduzido[50][4];
+    traducao(separado, banco_tradu, traduzido);
+    for(int i = 0; i < 50 && traduzido[i][0] != '\0'; i++) {
+        printf("%s", traduzido[i]);
+    }
+    printf("\n");
 }
